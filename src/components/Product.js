51564267b14101a3354a5dcaffcc66e6bf3bd7e5
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 23:21:07 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,30 @@
 import React from 'react' ;
+
+export default class Product extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <h3>
+          {this.props.name}
+        </h3>
+        <p>
+          producer:
+          {this.props.producer}
+        </p>
+        <p>
+          hasWaterMark:
+          {this.props.hasWaterMark}
+        </p>
+        <p>
+          color:
+          {this.props.color}
+        </p>
+        <p>
+          weight:
+          {this.props.weight}
+        </p>
+      </div>
+    )
+  }
+} 
